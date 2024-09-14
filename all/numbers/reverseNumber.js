4d5c8c14d48bc48f96de4bb32671a105a1c132d0
biswa7064 <biswaranjan16856@gmail.com>
2024-09-14 23:48:15 +0530

    feat: reverse number by converting it to string

--- all/numbers/reverseNumber.js
+++ all/numbers/reverseNumber.js
@@ -1,3 +1,4 @@
+// Method:1 - Reverse number without converting it to string
 export function reverseNumber(num) {
   let reversed = 0
   const sign = num < 0 ? -1 : 1
@@ -8,5 +9,22 @@ export function reverseNumber(num) {
   return reversed * sign
 }
 
+// Method:2 - Reverse number by converting it to string
+export function reverseNumberConvertingString(num) {
+  if (!num) return 0
+  const numStr = num.toString()
+  let reversed = ""
+  let length = numStr.length - 1
+  while (length >= 0) {
+    reversed += numStr.charAt(length)
+    length--
+  }
+  return Number(reversed)
+}
+
 const test1 = 238976
 console.log(`Reversed number of ${test1}`, reverseNumber(test1))
+console.log(
+  `Reversed number of ${test1} using Method:2`,
+  reverseNumberConvertingString(test1)
+)
