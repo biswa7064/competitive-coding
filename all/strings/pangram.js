0ce458404b6430c6e21c5775f3c7156c105e74ec
biswa7064 <biswaranjan16856@gmail.com>
2025-02-28 00:44:49 +0530

    refactor: plain object for pangram problem

--- all/strings/pangram.js
+++ all/strings/pangram.js
@@ -15,3 +15,21 @@ const test3 = "Hello, World!"
 console.log(`is "${test1}" Pangram? `, isPangram(test1))
 console.log(`is "${test2}" Pangram? `, isPangram(test2))
 console.log(`is "${test3}" Pangram? `, isPangram(test3))
+
+// using plain object
+const isPangramUsingPlainObj = (str) => {
+  const set = {}
+  const regex = /[a-z]/
+  for (const char of str.toLowerCase()) {
+    if (!regex.test(char) || set[char]) continue
+    set[char] = (set[char] || 0) + 1
+  }
+  return Object.keys(set).length === 26 ? "Yes" : "No"
+}
+
+const testPO1 = "When zombies arrive, quickly fax Judge Pat"
+const testPO2 = "The quick brown fox jumps over the lazy dog"
+const testPO3 = "Hello, World!"
+console.log(`is "${testPO1}" Pangram? `, isPangramUsingPlainObj(testPO1)) // true
+console.log(`is "${testPO2}" Pangram? `, isPangramUsingPlainObj(testPO2)) // true
+console.log(`is "${testPO3}" Pangram? `, isPangramUsingPlainObj(testPO3)) // false
