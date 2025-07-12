1402707a84af5a6fb9f75c8f1c675df401768848
biswa7064 <biswaranjan16856@gmail.com>
2025-07-12 13:18:10 +0530

    refactor: example for triplet using two-pointer

--- all/algo/two-pointer/triplet.js
+++ all/algo/two-pointer/triplet.js
@@ -21,7 +21,12 @@ function triplet(numArr, target) {
   return result
 }
 
-//  Example
-const arr2 = [1, 2, 3, 4, 5, 6]
-const target2 = 10
-console.log(`Triplets that add up to ${target2}:`, triplet(arr2, target2)) // Output: [[1, 3, 6], [1,4,5], [2,3,5]]
+//  Example:1
+const arr1 = [1, 2, 3, 4, 5, 6]
+const target1 = 10
+console.log(`Triplets that add up to ${target1}:`, triplet(arr1, target1)) // Output: [[1, 3, 6], [1,4,5], [2,3,5]]
+
+//  Example:2
+const arr2 = [1, 4, 6, 8, 10, 45]
+const target2 = 22
+console.log(`Triplets that add up to ${target2}:`, triplet(arr2, target2)) // Output: [[4,8,10]]
