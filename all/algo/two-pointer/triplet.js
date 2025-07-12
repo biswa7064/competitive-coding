af69de62d260febd53d959336e4bf885808fb53f
biswa7064 <biswaranjan16856@gmail.com>
2025-07-12 13:13:30 +0530

    feat: triplet using two-pointer

---
+++ all/algo/two-pointer/triplet.js
@@ -0,0 +1,27 @@
+// Effective if the input array is sorted
+// Time Complexity: O(n^2)
+function triplet(numArr, target) {
+  const result = []
+  for (let i = 0; i < numArr.length - 2; i++) {
+    let left = i + 1,
+      right = numArr.length - 1
+    while (left < right) {
+      const sum = numArr[i] + numArr[left] + numArr[right]
+      if (sum === target) {
+        result.push([numArr[i], numArr[left], numArr[right]])
+        left++
+        right--
+      } else if (sum < target) {
+        left++
+      } else {
+        right--
+      }
+    }
+  }
+  return result
+}
+
+//  Example
+const arr2 = [1, 2, 3, 4, 5, 6]
+const target2 = 10
+console.log(`Triplets that add up to ${target2}:`, triplet(arr2, target2)) // Output: [[1, 3, 6], [1,4,5], [2,3,5]]
