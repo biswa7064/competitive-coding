68aceef21fc7797d35969ac5dee09c0e2492a7d6
biswa7064 <biswaranjan16856@gmail.com>
2025-08-03 16:27:52 +0530

    refactor: simplify logic for remove duplicate in lexographic order greedy

--- all/dsa/greedy/removeDuplicateInLexographicOrder.js
+++ all/dsa/greedy/removeDuplicateInLexographicOrder.js
@@ -3,34 +3,29 @@
 
 function removeDuplicateLetters(s) {
   let lastFound = {},
-    start = 0,
-    end = s.length - 1,
     stack = [],
     seen = new Set()
-  for (let i = 0; i <= end; i++) {
+  for (let i = 0; i < s.length; i++) {
     // store the last occurrence of each character
     lastFound[s[i]] = i
   }
-  while (start <= end) {
+  for (let start = 0; start < s.length; start++) {
     const item = s[start]
+    const stackTopItem = stack[stack.length - 1]
     // if the item is already seen, skip it
-    if (seen.has(item)) {
-      start++
-      continue
-    }
+    if (seen.has(item)) continue
     // if the item is not seen, check if we can pop from the stack
     // we can pop if the stack is not empty, the current item is smaller than the last item in the stack,
     // and the last occurrence of the last item in the stack is after the current start index
     while (
       stack.length &&
-      item < stack[stack.length - 1] &&
-      start < lastFound[stack[stack.length - 1]]
+      item < stackTopItem &&
+      start < lastFound[stackTopItem]
     ) {
       seen.delete(stack.pop()) // remove item from seen set and stack
     }
     seen.add(item)
     stack.push(item)
-    start++
   }
   return stack.join("")
 }
