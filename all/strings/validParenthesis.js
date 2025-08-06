238c69cb3c0fe4861577b53dd726e7811e9c9612
biswa7064 <biswaranjan16856@gmail.com>
2025-08-07 01:30:03 +0530

    feat: valid parenthesis string problem

---
+++ all/strings/validParenthesis.js
@@ -0,0 +1,28 @@
+const brackets = {
+  "(": ")",
+  "{": "}",
+  "[": "]",
+}
+function validParenthesis(s) {
+  const bracketStack = []
+  for (let i = 0; i < s.length; i++) {
+    if (["(", "{", "["].includes(s[i])) {
+      bracketStack.push(s[i])
+    } else {
+      // if no open bracket is found then return false and the stack should be empty
+      if (bracketStack.length === 0) return false
+      // check if the closing bracket matches the last opened bracket
+      // if no matches found then return false
+      const lastStackItem = bracketStack.pop()
+      if (s[i] !== brackets[lastStackItem]) return false
+    }
+  }
+  // if there are any unmatched opening brackets left in the stack then return false else true
+  return bracketStack.length === 0
+}
+
+// Example Usage
+console.log(validParenthesis("()")) // true
+console.log(validParenthesis("({[]})")) // true
+console.log(validParenthesis("({[}])")) // false
+console.log(validParenthesis("[")) // false
