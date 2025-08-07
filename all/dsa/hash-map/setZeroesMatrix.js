b48497804f09e5c5914a32f2dace1c3598e831ba
biswa7064 <biswaranjan16856@gmail.com>
2025-08-07 18:51:29 +0530

    feat: set zeroes for rows and cols when fouund zero hash-map

---
+++ all/dsa/hash-map/setZeroesMatrix.js
@@ -0,0 +1,37 @@
+function setZeroes(matrix) {
+  const rows = new Set()
+  const cols = new Set()
+  for (let i = 0; i < matrix.length; i++) {
+    for (let j = 0; j < matrix[0].length; j++) {
+      if (matrix[i][j] === 0) {
+        rows.add(i)
+        cols.add(j)
+      }
+    }
+  }
+
+  for (let i = 0; i < matrix.length; i++) {
+    for (let j = 0; j < matrix[0].length; j++) {
+      if (rows.has(i) || cols.has(j)) {
+        matrix[i][j] = 0
+      }
+    }
+  }
+  return matrix
+}
+
+// Example Usage
+const matrix = [
+  [1, 1, 1],
+  [1, 0, 1],
+  [1, 1, 1],
+]
+console.log(setZeroes(matrix)) // [[1,0,1],[0,0,0],[1,0,1]]
+
+const matrix2 = [
+  [0, 1, 2, 0],
+  [3, 4, 5, 2],
+  [1, 3, 1, 5],
+]
+
+console.log(setZeroes(matrix2)) // [[0,0,0,0],[0,4,5,0],[0,3,1,0]]
